b10adea69af1b63485c6453864100423905136a4
Jai Mitch <jaimitch62@gmail.com>
2022-08-16 15:21:49 -0400

    Add files via upload

---
+++ YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("JJJJJJJ  M     M");
+        System.out.println("   J     MM   MM");
+        System.out.println("   J     M  M  M");
+        System.out.println("   J     M     M");
+        System.out.println("   J     M     M");
+        System.out.println("J  J     M     M");
+        System.out.println(" J       M     M");//print initials here. 
+    }
+}
